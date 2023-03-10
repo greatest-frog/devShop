59bbab8c8de7d9768d9e2366d51cb8a879fc57f6
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2023-03-10 15:09:50 +0300

    Remove filters from shop page

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -255,7 +255,7 @@ const Shop = ({ goods, addGoods }) => {
                   </svg>
                 )}
               </button>
-              <button
+              {/*<button
                 onClick={() => {
                   if (filtering) {
                     setFiltering(false);
@@ -275,7 +275,7 @@ const Shop = ({ goods, addGoods }) => {
                     <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                   </svg>
                 )}
-              </button>
+                </button>*/}
             </div>
           </div>
           <Filter
