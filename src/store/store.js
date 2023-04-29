befe9b605574db780bf93783ccb91c04576a3a51
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2023-04-29 18:47:37 +0300

    Add localStorage saving for cart

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../reducers/cartReducer";
 
+function localStorageMiddleware({ getState }) {
+  return (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem("applicationState", JSON.stringify(getState()));
+    return result;
+  };
+}
+
+const reHydrateStore = () => {
+  if (localStorage.getItem("applicationState") !== null) {
+    return JSON.parse(localStorage.getItem("applicationState"));
+  }
+};
+
 const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
+  preloadedState: reHydrateStore(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 });
 
 export default store;
